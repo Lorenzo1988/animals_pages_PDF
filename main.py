88f7f2e0983582a93f66cab9a719591e8a10860a
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-05 22:42:41 +0200

    Inseriti nomi animali inizio pagina

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ print(filepaths)
 #SELEZIONE_FILES
 
 for i, filepath in enumerate(filepaths):
-    print(i)
     nome_file = Path(filepath).stem
-    print(nome_file)
+    #PDF FILES
+    pdf = FPDF(orientation="P",format="A4",unit="mm")
+    pdf.add_page()
+    pdf.set_font(family="Times",size=16,style="B")
+    pdf.cell(w=50,h=8,txt= f"{str(nome_file).title()}")
+    pdf.output(path_output+"pdf_"+str(nome_file)+".pdf")
