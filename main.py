f942f3cfb072518eca7b441cbf45a0a54a9aa2a6
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-05 22:16:44 +0200

    Aggiunstato venv. Inseriti files input. Inizio sviluppo main

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fpdf import FPDF
 import glob
-
+from pathlib import Path
 #PATH
 path_input = "input_files/"
 path_output="output_files/"
 
-filepaths = glob.glob("input_files")
+filepaths = glob.glob("input_files/*.txt")
 print(filepaths)
 
 #SELEZIONE_FILES
+
+for i, filepath in enumerate(filepaths):
+    print(i)
+    nome_file = Path(filepath).stem
+    print(nome_file)
